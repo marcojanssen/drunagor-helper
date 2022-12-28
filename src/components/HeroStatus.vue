49ac6aed717642a50c1856022451324a14665fa0
Marco Janssen <dev@marcojanssen.net>
2022-12-28 21:15:37 +0100

    Implemented new select component

---
+++ src/components/HeroStatus.vue
@@ -0,0 +1,115 @@
+<script setup lang="ts">
+import { ref, computed, watch } from "vue";
+import { PartyStore } from "@/store/PartyStore";
+import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from "@headlessui/vue";
+import { CheckIcon, ChevronUpDownIcon, XMarkIcon } from "@heroicons/vue/20/solid";
+import { StatusDataStore } from "@/data/store/StatusDataStore";
+
+const partyStore = PartyStore();
+const statusStore = StatusDataStore();
+const statuses = statusStore.findAll();
+
+const props = defineProps<{
+  heroId: string;
+}>();
+const statusIds = ref([] as string[]);
+statusIds.value = partyStore.find(props.heroId).statusIds;
+
+let filteredStatuses = computed(() =>
+  query.value === ""
+    ? statuses
+    : statuses.filter((status) =>
+        status.name.toLowerCase().replace(/\s+/g, "").includes(query.value.toLowerCase().replace(/\s+/g, ""))
+      )
+);
+
+let query = ref("");
+
+function clearSelection() {
+  statusIds.value = [];
+  query.value = "";
+}
+
+watch(statusIds, (newStatusIds) => {
+  partyStore.find(props.heroId).statusIds = newStatusIds;
+});
+</script>
+
+<template>
+  <div>
+    <h2 class="text-white">Status(es)</h2>
+    <p class="text-sm text-gray-500">Statuses are removed during the camp phase.</p>
+  </div>
+  <div class="w-full">
+    <Combobox v-model="statusIds" nullable multiple>
+      <div class="relative mt-1">
+        <div class="relative w-full cursor-default overflow-hidden rounded-lg text-left">
+          <ComboboxButton as="div" class="flex">
+            <ComboboxInput
+              class="w-full bg-neutral py-2 pl-3 pr-16 leading-5 focus:ring-0"
+              placeholder="Select status"
+              @change="query = $event.target.value"
+            />
+          </ComboboxButton>
+          <button
+            @click="clearSelection"
+            v-if="statusIds.length > 0"
+            class="absolute inset-y-0 right-7 flex items-center pr-2"
+          >
+            <XMarkIcon class="h-5 w-5 text-white" />
+          </button>
+          <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon class="h-5 w-5 text-white" />
+          </ComboboxButton>
+        </div>
+
+        <ComboboxOptions
+          class="bg-neutral -top-2 transform -translate-y-full absolute mt-1 max-h-60 w-full overflow-auto rounded-md py-1"
+        >
+          <div v-if="filteredStatuses.length === 0" class="relative cursor-default select-none py-2 px-4 text-white">
+            Nothing found.
+          </div>
+
+          <ComboboxOption
+            v-for="status in filteredStatuses"
+            as="template"
+            :key="status.id"
+            :value="status.id"
+            v-slot="{ selected, active }"
+          >
+            <li
+              class="relative cursor-pointer select-none py-2 pl-10 pr-4"
+              :class="{
+                'bg-emerald-500 text-white': active,
+                'text-white': !active,
+              }"
+            >
+              <span class="block truncate">
+                {{ status.name }}
+              </span>
+              <span
+                v-if="selected"
+                class="absolute inset-y-0 left-0 flex items-center pl-3"
+                :class="{ 'text-white': active, 'text-emerald-500': !active }"
+              >
+                <CheckIcon class="h-5 w-5" />
+              </span>
+            </li>
+          </ComboboxOption>
+        </ComboboxOptions>
+      </div>
+    </Combobox>
+  </div>
+  <template v-for="statusId in statusIds" :key="statusId">
+    <ul class="list-disc list-inside">
+      <li>
+        {{ statusStore.find(statusId)?.name }}
+        <span class="bg-base-200 p-4 block" v-if="statusStore.find(statusId)?.effect">
+          {{ statusStore.find(statusId)?.effect }}
+        </span>
+      </li>
+    </ul>
+  </template>
+</template>
+
+<style scoped></style>
