d96064f7387efd6f9dc6b560e017809bc72ecf1a
Marco Janssen <dev@marcojanssen.net>
2023-01-17 18:19:44 +0100

    Replaced drawer & colors

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { ConfigurationStore } from "./store/ConfigurationStore";
-import type { VariantId } from "./data/type/VariantId";
-import {
-  Bars3Icon,
-  Cog6ToothIcon,
-  ListBulletIcon,
-  QuestionMarkCircleIcon,
-  UserGroupIcon,
-} from "@heroicons/vue/24/solid";
-
-const drawerClose = ref(false);
-function closeDrawer() {
-  drawerClose.value = false;
-}
+import { ConfigurationStore } from "@/store/ConfigurationStore";
+import type { VariantId } from "@/data/type/VariantId";
+import MainMenu from "./components/MainMenu.vue";
 
 const configurationStore = ConfigurationStore();
 
@@ -36,64 +24,12 @@ configurationStore.enabledVariants = newVariants;
 </script>
 
 <template>
-  <div class="drawer">
-    <input id="menu-drawer" v-model="drawerClose" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col">
-      <div class="w-full navbar bg-neutral sticky top-0 z-50 drop-shadow">
-        <label for="menu-drawer" class="flex cursor-pointer">
-          <Bars3Icon class="h-6 w-6" />
-          <span class="pl-2">Menu</span>
-        </label>
-      </div>
-      <div class="container mx-auto lg:w-3/4 p-4">
-        <router-view> </router-view>
-      </div>
-    </div>
-    <div class="drawer-side">
-      <label for="menu-drawer" class="drawer-overlay bg-opacity-90"></label>
-      <ul class="menu w-80 flex flex-col pt-4 px-4 bg-base-300 drop-shadow">
-        <li>
-          <router-link class="flex" to="/" @click="closeDrawer">
-            <span>
-              <QuestionMarkCircleIcon class="h-7 w-7" />
-            </span>
-            <span>Randomizer</span>
-          </router-link>
-        </li>
-        <li>
-          <router-link class="flex" to="/party" @click="closeDrawer">
-            <span>
-              <UserGroupIcon class="h-7 w-7" />
-            </span>
-            <span>Party</span>
-          </router-link>
-        </li>
-        <li>
-          <router-link class="flex" to="/keyword" @click="closeDrawer">
-            <span>
-              <ListBulletIcon class="h-7 w-7" />
-            </span>
-            <span>Keyword <span class="text-emerald-300 text-xs font-semibold align-super">Beta</span></span>
-          </router-link>
-        </li>
-        <li>
-          <router-link class="flex" to="/configuration" @click="closeDrawer">
-            <span>
-              <Cog6ToothIcon class="h-7 w-7" />
-            </span>
-            <span>Content / Variant</span>
-          </router-link>
-        </li>
-      </ul>
-    </div>
+  <div class="w-full navbar bg-neutral sticky top-0 z-10 drop-shadow">
+    <MainMenu />
+  </div>
+  <div class="container mx-auto lg:w-3/4 p-4">
+    <router-view> </router-view>
   </div>
 </template>
 
-<style scoped>
-.router-link-active {
-  @apply bg-neutral;
-}
-a:active {
-  @apply bg-neutral text-white;
-}
-</style>
+<style scoped></style>
