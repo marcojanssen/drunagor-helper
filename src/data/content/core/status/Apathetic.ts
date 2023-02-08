345a9a34c73fae22a5d8b8e1e90a63b9cbe61bc4
Marco Janssen <dev@marcojanssen.net>
2023-02-08 19:43:56 +0100

    Apathetic has no effect

--- src/data/content/core/status/Apathetic.ts
+++ src/data/content/core/status/Apathetic.ts
@@ -3,6 +3,5 @@ import type { StatusData } from "@/data/repository/StatusData";
 export class Apathetic implements StatusData {
   id = "apathetic";
   name = "Apathetic";
-  effect =
-    "Whenever you take a Recall Action during your turn, you may retrieve a Scroll card from the Chest deck discard pile and use it immediately. This does not count as a “Use Consumable Item” Minor Action. If you do, after resolving that card, shuffle it back in the Chest deck instead of putting it again in the discard pile.";
+  effect = "";
 }
