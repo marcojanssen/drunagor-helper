89ff8ddc0b605eb94a159a29aba64e7ad94f175e
Marco Janssen <dev@marcojanssen.net>
2023-04-29 09:22:05 +0200

    Added final unfoldings

--- src/data/repository/campaign/apocalypse/StoryRecordUnfoldingRepository.ts
+++ src/data/repository/campaign/apocalypse/StoryRecordUnfoldingRepository.ts
@@ -4,119 +4,129 @@ import type { Unfolding } from "./Unfolding";
 export class StoryRecordUnfoldingRepository {
   private followers: Unfolding[] = [
     {
-      id: "",
+      id: "to-be-or-not-to-be",
       name: "To Be Or Not To Be?",
       act: 1,
     },
     {
-      id: "",
+      id: "carmillas-goodbye",
       name: "Carmilla's Goodbye",
       act: 1,
     },
     {
-      id: "",
+      id: "heroic-folks",
       name: "Heroic Folks",
       act: 1,
     },
     {
-      id: "",
+      id: "the-good-duke",
       name: "The Good Duke",
       act: 1,
     },
     {
-      id: "",
+      id: "truth-untold",
       name: "Truth Untold",
       act: 1,
     },
     {
-      id: "",
+      id: "luccanors-regret",
       name: "Luccanor's Regret",
       act: 1,
     },
     {
-      id: "",
+      id: "luccanors-redemption",
       name: "Luccanor's Redemption",
       act: 1,
     },
     {
-      id: "",
+      id: "no-ones-land",
       name: "No One's Land",
       act: 2,
     },
     {
-      id: "",
+      id: "gil-garoth-reborn",
       name: "Gil Garoth Reborn",
       act: 2,
     },
     {
-      id: "",
+      id: "the-last-dance",
       name: "The Last Dance",
       act: 2,
     },
     {
-      id: "",
+      id: "a-crown-with-no-jewels",
       name: "A Crown with no Jewels",
       act: 2,
     },
     {
-      id: "",
+      id: "the-desert-flower",
       name: "The Desert Flower",
       act: 2,
     },
     {
-      id: "",
+      id: "a-broken-man",
       name: "A Broken Man",
       act: 2,
     },
     {
-      id: "",
+      id: "a-hero-restored",
       name: "A Hero Restored",
       act: 2,
     },
     {
-      id: "",
+      id: "what-goes-around-comes-around",
       name: "What Goes Around, Comes Around",
       act: 3,
     },
     {
-      id: "",
+      id: "the-bargain",
       name: "The Bargain",
       act: 3,
     },
     {
-      id: "",
+      id: "the-earl-of-nothing",
       name: "The Earl of Nothing",
       act: 3,
     },
     {
-      id: "",
+      id: "connecting-dots",
       name: "Connecting Dots",
       act: 3,
     },
     {
-      id: "",
+      id: "the-bonds-uniting-us",
       name: "The Bonds Uniting Us",
       act: 3,
     },
     {
-      id: "",
+      id: "the-safeguard-corporation",
       name: "The Safeguard Corporation",
       act: 3,
     },
     {
-      id: "",
+      id: "crossroads",
       name: "Crossroads",
       act: 3,
     },
     {
-      id: "",
+      id: "tragic-fate",
       name: "Tragic Fate",
       act: 3,
     },
     {
-      id: "",
+      id: "survivors-survived",
       name: "Survivors Survived",
       act: 3,
+    },
+    {
+      id: "compromising",
+      name: "Comprimising",
+      act: 4,
+    },
+    {
+      id: "the-fallout",
+      name: "The Fallout",
+      act: 4,
     },
   ];
 
