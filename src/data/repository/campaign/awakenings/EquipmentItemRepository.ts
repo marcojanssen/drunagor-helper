d484eaa99979d7d5daebc1fd573d2975a113e30f
Marco Janssen <dev@marcojanssen.net>
2023-05-09 19:31:16 +0200

    Initial batch of items

---
+++ src/data/repository/campaign/awakenings/EquipmentItemRepository.ts
@@ -0,0 +1,139 @@
+import type {
+  ArmorItemData,
+  ConsumableItemData,
+  ItemData,
+  OffHandItemData,
+  WeaponItemData,
+} from "@/data/repository/ItemData";
+import {
+  instanceOfArmorCardData,
+  instanceOfConsumableCardData,
+  instanceOfOffHandCardData,
+  instanceOfWeaponCardData,
+} from "@/data/repository/ItemData";
+import type { ItemDataRepository } from "@/data/repository/ItemDataRepository";
+import type { ArmorType } from "@/data/type/ArmorType";
+import type { ConsumableType } from "@/data/type/ConsumableType";
+import type { ItemType } from "@/data/type/ItemType";
+import type { OffHandType } from "@/data/type/OffHandType";
+import type { WeaponType } from "@/data/type/WeaponType";
+import * as _ from "lodash-es";
+
+export class EquipmentItemRepository implements ItemDataRepository {
+  private cards = [
+    {
+      id: "scholars-robe",
+      name: "Scholar's Robe",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Armor",
+      armorTypes: ["Cloth"],
+    } as ArmorItemData,
+    {
+      id: "warworn-breastplate",
+      name: "Warworn Breastplate",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Armor",
+      armorTypes: ["Plate"],
+    } as ArmorItemData,
+    {
+      id: "veteran-cuirass",
+      name: "Veteran Cuirass",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Armor",
+      armorTypes: ["Leather"],
+    } as ArmorItemData,
+    {
+      id: "hunting-shortbow",
+      name: "Hunting Shortbow",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Weapon",
+      weaponTypes: ["Ranged"],
+    } as WeaponItemData,
+    {
+      id: "axe-of-the-steppes",
+      name: "Axe of the Steppes",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Weapon",
+      weaponTypes: ["Heavy"],
+    } as WeaponItemData,
+    {
+      id: "hellscarian-falchions",
+      name: "Hellscarian Falchion",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Weapon",
+      weaponTypes: ["Light"],
+    } as WeaponItemData,
+    {
+      id: "counselors-staff",
+      name: "Counselor's Staff",
+      content: "apocalypse",
+      cardType: "Starting Gear",
+      cardRank: null,
+      cardRarity: "Normal",
+      itemType: "Weapon",
+      weaponTypes: ["Implement"],
+    } as WeaponItemData,
+  ];
+
+  find(cardId: string): ItemData | undefined {
+    return _.find(this.cards, { id: cardId });
+  }
+
+  findAll(): ItemData[] {
+    return this.cards;
+  }
+
+  findByType(type: ItemType, subType: ArmorType | ConsumableType | OffHandType | WeaponType | null = null): ItemData[] {
+    const cards: ItemData[] = [];
+
+    const isSubType = function (
+      item: ItemData,
+      st: ArmorType | ConsumableType | OffHandType | WeaponType | null
+    ): boolean {
+      if (st === null) {
+        return true;
+      }
+
+      if (instanceOfArmorCardData(item)) {
+        return (item as ArmorItemData).armorTypes.some((value) => value === st);
+      }
+      if (instanceOfConsumableCardData(item)) {
+        return (item as ConsumableItemData).consumableType === st;
+      }
+      if (instanceOfOffHandCardData(item)) {
+        return (item as OffHandItemData).offHandTypes.some((value) => value === st);
+      }
+      if (instanceOfWeaponCardData(item)) {
+        return (item as WeaponItemData).weaponTypes.some((value) => value === st);
+      }
+      return false;
+    };
+
+    this.cards.forEach((card) => {
+      const c = card as ItemData;
+      if (c.itemType === type && (subType == null || isSubType(c, subType))) {
+        cards.push(c);
+      }
+    });
+
+    return cards;
+  }
+}
