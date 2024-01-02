1c1e04c514938162cdb6eb9027af5cfe3df41326
Marco Janssen <2293642+marcojanssen@users.noreply.github.com>
2024-01-02 20:35:33 +0100

    Added anchors for direct linking keywords

--- src/views/KeywordView.vue
+++ src/views/KeywordView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
 import { KeywordDataRepository } from "@/data/repository/KeywordDataRepository";
 import BaseListSearch from "@/components/BaseListSearch.vue";
 import BaseList from "@/components/BaseList.vue";
@@ -7,8 +8,15 @@ import KeywordListItem from "@/components/KeywordListItem.vue";
 import { marked } from "marked";
 import BaseDivider from "@/components/BaseDivider.vue";
 
+const route = useRoute();
 const keywordDataRepository = new KeywordDataRepository();
 const keywords = keywordDataRepository.findAll();
+
+let preselectedKeyword = "";
+if (route.hash) {
+  preselectedKeyword = route.hash.toString().replace(/[#]/g, "");
+  preselectedKeyword = preselectedKeyword.replace(/[-]/g, " ");
+}
 
 let filteredKeyword = computed(() =>
   query.value === ""
@@ -19,6 +27,7 @@ let filteredKeyword = computed(() =>
 );
 
 let query = ref("");
+query.value = preselectedKeyword;
 </script>
 
 <template>
@@ -28,17 +37,20 @@ let query = ref("");
       id="keyword-search"
       @search="query = $event"
       class="z-10 bg-base-300 inset-16 w-full border-0 shadow-xl"
+      :value="query"
     />
     <BaseList id="keyword-list" class="grid gap-3 w-full">
       <template v-for="keyword in filteredKeyword" :key="keyword.id">
-        <KeywordListItem>
-          <template #default>
-            {{ keyword.keyword }}
-          </template>
-          <template #description>
-            <span v-html="marked.parse(keyword.description)"></span>
-          </template>
-        </KeywordListItem>
+        <router-link :to="'/keyword#' + keyword.id">
+          <KeywordListItem :opened="preselectedKeyword === keyword.id">
+            <template #default>
+              {{ keyword.keyword }}
+            </template>
+            <template #description>
+              <span v-html="marked.parse(keyword.description)"></span>
+            </template>
+          </KeywordListItem>
+        </router-link>
       </template>
     </BaseList>
   </div>
