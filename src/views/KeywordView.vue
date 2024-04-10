75adeeba5e05973a4acc8588daa5275eea012242
marcojanssen <2293642+marcojanssen@users.noreply.github.com>
2024-04-10 20:15:05 +0200

    Fixes translation errors

--- src/views/KeywordView.vue
+++ src/views/KeywordView.vue
@@ -8,7 +8,9 @@ import KeywordListItem from "@/components/KeywordListItem.vue";
 import { marked } from "marked";
 import BaseDivider from "@/components/BaseDivider.vue";
 import { ConfigurationStore } from "@/store/ConfigurationStore";
+import { useI18n } from "vue-i18n";
 
+const { t } = useI18n();
 const route = useRoute();
 const keywordDataRepository = new KeywordDataRepository();
 const configurationStore = ConfigurationStore();
@@ -36,7 +38,7 @@ query.value = preselectedKeyword;
 
 <template>
   <div class="grid place-items-center w-full">
-    <BaseDivider>Keywords</BaseDivider>
+    <BaseDivider>{{ t("menu.keyword") }}</BaseDivider>
     <BaseListSearch
       id="keyword-search"
       @search="query = $event"
