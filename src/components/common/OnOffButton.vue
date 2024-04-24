8470ee2f46c5712475a6931d551e3f0e4c9a0d7c
erikblessman <erik.blessman+gh@gmail.com>
2024-04-23 21:19:07 -0600

    OnOffButton

---
+++ src/components/common/OnOffButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { flag, text } = defineProps(["flag", "text"]);
+</script>
+
+<template>
+    <div class="flex flex-wrap bg-base-100 justify-center py-1 px-4 mx-1 rounded-lg"
+        :class="flag ? 'bg-red-600' : 'bg-green-600'">
+        <slot></slot>: {{ flag ? 'ON' : 'OFF' }}
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/InitiativeView.vue
+++ src/views/InitiativeView.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
+import BaseDivider from "@/components/BaseDivider.vue";
+import OnOffButton from "@/components/common/OnOffButton.vue";
+import {
+    PlusIcon
+} from "@heroicons/vue/24/solid";
+const foo1 = true;
+const foo2 = false;
 </script>
 
 <template>
-    <h1>Hello World</h1>
+    <div class="grid place-items-center w-full">
+        <BaseDivider>Initiative</BaseDivider>
+        <div class="w-full flex">
+            <PlusIcon class="w-8" />
+            <OnOffButton :flag="foo1">Auto Confirm</OnOffButton>
+            <OnOffButton :flag="foo2">Default HP</OnOffButton>
+        </div>
+    </div>
 </template>
