4f927b65b0cd5c03718a73b0b2416d213b4e718f
erikblessman <erik.blessman+gh@gmail.com>
2024-04-28 23:12:56 -0600

    Resolved BaseListItem warnings

--- src/components/initiative/MonsterInitiative.vue
+++ src/components/initiative/MonsterInitiative.vue
@@ -37,33 +37,31 @@ const { decrementHp, incrementHp, removeMonster } = useInitiativeStore();
         <div class="col-span-11">
             <template v-for="(monster) in props.monsters.sort((a, b) => a.msTimestamp - b.msTimestamp)"
                 :key="monster.msTimestamp">
-                <BaseListItem>
-                    <div class="grid grid-flow-col auto-cols-max" v-touch:swipe.right="() => incrementHp(monster)"
-                        v-touch:swipe.left="() => decrementHp(monster)">
-                        <img :src="monster.images.big" @click="emit('open-details', monster)"
-                            :style="'border-color:' + monster.baseColor + ';'"
-                            class="bg-white border-8 rounded-full shadow dark:bg-gray-800"
-                            :class="monster.size == 'large' ? 'w-32' : 'w-24'" />
-                        <div>
-                            <div class="font-semibold text-lg">
-                                {{ monster.name }} ({{ monster.color }})
-                            </div>
-                            <div class="grid grid-flow-col auto-cols-max">
-                                <div class="grid w-12">
-                                    <div class="col-start-1 row-start-1 justify-center">
-                                        <HeartIcon class="fill-red-500 w-12 self-center" />
-                                    </div>
-                                    <div
-                                        class="col-start-1 row-start-1 self-center text-center font-semibold text-red self-center">
-                                        {{ monster.hp }}
-                                    </div>
+                <div class="grid grid-flow-col auto-cols-max" v-touch:swipe.right="() => incrementHp(monster)"
+                    v-touch:swipe.left="() => decrementHp(monster)">
+                    <img :src="monster.images.big" @click="emit('open-details', monster)"
+                        :style="'border-color:' + monster.baseColor + ';'"
+                        class="bg-white border-8 rounded-full shadow dark:bg-gray-800"
+                        :class="monster.size == 'large' ? 'w-32' : 'w-24'" />
+                    <div>
+                        <div class="font-semibold text-lg">
+                            {{ monster.name }} ({{ monster.color }})
+                        </div>
+                        <div class="grid grid-flow-col auto-cols-max">
+                            <div class="grid w-12">
+                                <div class="col-start-1 row-start-1 justify-center">
+                                    <HeartIcon class="fill-red-500 w-12 self-center" />
                                 </div>
-                                <Conditions :monster="monster" />
-                                <TrashIcon class="fill-gray-600 w-12" @click="removeMonster(monster)" />
+                                <div
+                                    class="col-start-1 row-start-1 self-center text-center font-semibold text-red self-center">
+                                    {{ monster.hp }}
+                                </div>
                             </div>
+                            <Conditions :monster="monster" />
+                            <TrashIcon class="fill-gray-600 w-12" @click="removeMonster(monster)" />
                         </div>
                     </div>
-                </BaseListItem>
+                </div>
             </template>
         </div>
     </div>
