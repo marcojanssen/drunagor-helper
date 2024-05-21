9a98e2e2aeab69a5ccb45d47a3f85100eb800c71
Sascha <skleiber@gmail.com>
2024-05-21 18:41:36 +0200

    A few more translations for sequential adventure-page

--- src/components/CampaignName.vue
+++ src/components/CampaignName.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { CampaignStore } from "@/store/CampaignStore";
 import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 
 const props = defineProps<{
   campaignId: string;
 }>();
+const { t } = useI18n();
 
 const campaignStore = CampaignStore();
 const campaign = campaignStore.find(props.campaignId);
@@ -17,7 +19,7 @@ watch(name, async (newName) => {
 
 <template>
   <div class="p-4 grid gap-2">
-    <span class="w-48">Party name:</span>
+    <span class="w-48">{{ t("text.party-name") }}</span>
     <input type="text" id="campaign-log-name" v-model="name" class="w-full bg-base-100 focus:ring-0 rounded-lg" />
   </div>
 </template>
